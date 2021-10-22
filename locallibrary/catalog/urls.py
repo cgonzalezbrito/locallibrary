a5b61781577c33573d238a430238a4fc70820be1
cgonzalezbrito <cgonzalezbrt@gmail.com>
2021-10-22 08:54:21 -0500

    [ADD] forms

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
         path('books/', views.BookListView.as_view(), name='books'),
         path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
         path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+        path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
 
+urlpatterns += [
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
